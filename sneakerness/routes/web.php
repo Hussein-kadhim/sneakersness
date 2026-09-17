ad625159f849a678f9a7d92218f92aa3e9da76a3
Hussein-kadhim <h-kadhim@outlook.com>
2026-09-17 11:51:46 +0200

    verkoper controller en routes toegevoegd

--- sneakerness/routes/web.php
+++ sneakerness/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VerkoperController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [VerkoperController::class, 'index'])->name('home');
+Route::get('/verkopers', [VerkoperController::class, 'index'])->name('verkopers.index');
+Route::resource('verkopers', VerkoperController::class)->except(['index']);
+
+
+Route::get('/dashboard', function () {
+    return redirect()->route('verkopers.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
