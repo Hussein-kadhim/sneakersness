59c944a2c2e5c4cf8db4b510cffdfcb0836ea6dc
Hussein-kadhim <h-kadhim@outlook.com>
2026-09-17 11:51:11 +0200

    inloggen en registreren gemaakt

---
+++ sneakerness/resources/views/components/primary-button.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center px-4 py-2.5 bg-orange-500 border border-transparent rounded-lg font-semibold text-sm text-white hover:bg-orange-600 focus:bg-orange-600 active:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 transition ease-in-out duration-150 shadow-sm']) }}>
+    {{ $slot }}
+</button>
