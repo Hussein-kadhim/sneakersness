5eaf54753a866135d97b44de01ba5a471fdde8a7
mouiz elmarini <344250@student.mboutrecht.nl>
2026-09-18 14:46:13 +0200

    feat(stands): add stand overview statistics to controller

--- sneakerness/app/Http/Controllers/StandController.php
+++ sneakerness/app/Http/Controllers/StandController.php
@@ -10,10 +10,25 @@ class StandController extends Controller
 {
     public function index(Request $request): View
     {
+        $totalStands = Stand::count();
+        $countAAPlus = Stand::where('StandType', 'AA+')->count();
+        $countAA = Stand::where('StandType', 'AA')->count();
+        $countA = Stand::where('StandType', 'A')->count();
+        $rentedStandsCount = Stand::where('VerhuurdStatus', 1)->count();
+        $availableStandsCount = Stand::where('VerhuurdStatus', 0)->count();
+
         $query = Stand::query()->with('verkoper');
 
         $stands = $query->orderBy('Id', 'asc')->paginate(6)->withQueryString();
 
-        return view('stands.index', compact('stands'));
+        return view('stands.index', compact(
+            'stands',
+            'totalStands',
+            'countAAPlus',
+            'countAA',
+            'countA',
+            'rentedStandsCount',
+            'availableStandsCount'
+        ));
     }
 }
